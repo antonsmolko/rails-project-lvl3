ae3134d946baec5ab0716c502dbfe07e1225d4e2
Anton Smolko <asmolko@yandex.ru>
2021-12-13 00:35:53 +0300

    step 6: completed: fix rubocop errors

--- db/schema.rb
+++ db/schema.rb
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_12_05_103246) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
