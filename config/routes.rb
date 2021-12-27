fa81b99ec5f7ff1846ea7d5c2f64688190867161
Anton Smolko <asmolko@yandex.ru>
2021-12-27 22:48:36 +0300

    step hexlet auto tests: fix errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, skip: :omniauth_callbacks
-
-  devise_scope :user do
-    match '/users/auth/:action/callback',
-          constraints: { action: /github/ },
-          to: 'web/omniauth_callbacks#github',
-          as: :callback_auth,
-          via: %i[get post]
-  end
-
+  devise_for :users, controllers: { omniauth_callbacks: 'web/omniauth_callbacks' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope module: :web do
     root 'home#index'
