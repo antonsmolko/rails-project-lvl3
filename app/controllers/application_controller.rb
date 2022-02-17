bd67f4765d7cb61d13ca6c6714ad429910eb9c75
Anton Smolko <asmolko@yandex.ru>
2022-02-17 23:43:59 +0300

    replace includes AuthManagement and Pundit in Web::ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  include AuthManagement
-  include Pundit
-
-  helper_method :signed_in?, :sign_out
-
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-
-  private
-
-  def user_not_authorized
-    redirect_to root_path, alert: t('notice.auth.not_authorized')
-  end
 end
--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class Web::ApplicationController < ApplicationController
+  helper_method :signed_in?, :sign_out
+
+  include AuthManagement
+  include Pundit
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 end
